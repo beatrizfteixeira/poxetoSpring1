eee601ef63749a34a7fe76b4e065567a4d884d50
beatrizfteixeira <biaft03@protonmail.com>
2023-02-11 07:44:23 -0300

    criando pacote para o projeto

---
+++ projetoCurso/poxetofinances/src/test/java/poxetofinances/com/example/poxetofinances/PoxetofinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package poxetofinances.com.example.poxetofinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoxetofinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
